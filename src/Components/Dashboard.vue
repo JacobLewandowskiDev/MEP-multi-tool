24fbee8717f2315410c26af3f72a658c6e3e2c35
unknown <lewandowskijakub9@gmail.com>
2026-03-22 02:49:44 +0100

    This commit is a WIP on the dashboard UI

--- src/Components/Dashboard.vue
+++ src/Components/Dashboard.vue
@@ -1,10 +1,21 @@
+<script setup>
+import TeamList from './dashboard tools/TeamList.vue';
+
+</script>
+
 <template>
     <div class="dashboard">
         <div class="dashboard__tool dashboard__tool--full">
             <h2>Workload and Team Summary</h2>
         </div>
         <!-- This will show the number of all projects, active projects, on hold projects, and a number of upcoming deadlines, upcoming employee PTO-->
-        <div class="dashboard__tool">Team List</div> 
+        <div class="dashboard__tool">Team List
+            <div class="teamList__labels">
+                <span>Name</span>
+                <span>Workload</span>
+            </div>
+            <TeamList />
+        </div> 
         <div class="dashboard__tool">Upcoming Deadlines</div>
         <div class="dashboard__tool">Placeholder</div>
     </div>
@@ -14,11 +25,13 @@
 .dashboard {
     display: grid;
     grid-template-columns: repeat(4, 1fr); 
+    grid-template-rows: auto minmax(0, 40vh); 
     gap: 1rem; 
     width: 100%;
     height: 100%;
     padding: 1rem;
     box-sizing: border-box;
+    text-align: center;
 }
 
 .dashboard__tool {
@@ -26,10 +39,12 @@
     border: var(--tool-border);
     padding: 1rem;
     border-radius: 0.5rem;
-    text-align: center;
     color: var(--tool-label-color);
-    font-size: 1.1rem;
     font-weight: bold;
+    font-size: 1.1rem;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
 }
 
 .dashboard__tool--full {
@@ -38,5 +53,19 @@
 
 .dashboard__tool:nth-child(3) {
     grid-column: span 2;
+}
+
+.teamList__labels {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    padding: .5rem .25rem 0;
+    width: 85%;
+    margin: 0 auto;
+    font-size: 0.9rem;
+}
+
+.teamList__labels span:last-child {
+    justify-self: end;
 }
 </style>
