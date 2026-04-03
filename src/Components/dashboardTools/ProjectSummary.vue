f2f5e7a0004424e959c1ea5af66e99c25f1273e7
unknown <lewandowskijakub9@gmail.com>
2026-04-03 03:29:10 +0200

    This commit is a WIP on the add new project form

--- src/Components/dashboardTools/ProjectSummary.vue
+++ src/Components/dashboardTools/ProjectSummary.vue
@@ -26,7 +26,14 @@ const setActive = (index) => {
             <h3>Project Summary</h3>
             
             <div class="summary__button-wrapper">
-                <button class="summary__add">ADD PROJECT</button>
+                <button class="summary__add"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                    <path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 160C544 124.7 515.3 96 480 96L160 96zM296 408L296 344L232 344C218.7 344 208 333.3 208 320C208 306.7 218.7 296 232 296L296 296L296 232C296 218.7 306.7 208 320 208C333.3 208 344 218.7 344 232L344 296L408 296C421.3 296 432 306.7 432 320C432 333.3 421.3 344 408 344L344 344L344 408C344 421.3 333.3 432 320 432C306.7 432 296 421.3 296 408z"/>
+                </svg>
+                    ADD PROJECT</button>
+                <button class="summary__save">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+                        <path d="M160 96C124.7 96 96 124.7 96 160L96 480C96 515.3 124.7 544 160 544L480 544C515.3 544 544 515.3 544 480L544 237.3C544 220.3 537.3 204 525.3 192L448 114.7C436 102.7 419.7 96 402.7 96L160 96zM192 192C192 174.3 206.3 160 224 160L384 160C401.7 160 416 174.3 416 192L416 256C416 273.7 401.7 288 384 288L224 288C206.3 288 192 273.7 192 256L192 192zM320 352C355.3 352 384 380.7 384 416C384 451.3 355.3 480 320 480C284.7 480 256 451.3 256 416C256 380.7 284.7 352 320 352z"/>
+                    </svg> SAVE CHANGES</button>
             </div>
         </div>
         <div class="summary__table">
@@ -99,10 +106,9 @@ const setActive = (index) => {
     border-right: var(--tool-border);
 }
 
-.summary__tabs__button, .summary__add {
+.summary__tabs__button, .summary__add, .summary__save {
     flex: 1; 
     min-width: 0;
-    
     background-color: var(--tool-inner-container-color);
     color: #fff;
     border: var(--tool-border);
@@ -110,7 +116,6 @@ const setActive = (index) => {
     padding: .5rem;
     margin: .3rem;
     transition: all .2s ease;
-    
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
@@ -118,23 +123,38 @@ const setActive = (index) => {
     font-weight: bold;
 }
 
-.summary__add {
+.summary__add, .summary__save {
     background-color: var(--primary-color);
     color: #000;
     font-weight: bold;
     font-size: 1rem;
     flex: 0 1 auto;
     padding: 0.75rem 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
-.summary__tabs__button:hover, .summary__add:hover {
+.summary__save {
+    background-color: var(--button-secondary-color);
+    cursor: pointer;
+}
+
+.summary__add svg, .summary__save svg {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-right: 0.25rem;
+    text-align: center;
+}
+
+.summary__tabs__button:hover, .summary__add:hover, .summary__save:hover {
     background-color: var(--primary-color-hover);
     cursor: pointer;
 }
 
 .summary__tabs__button.active {
     background-color: var(--primary-color);
-    color: #fff;
+    color: #000;
 }
 
 .summary__table::-webkit-scrollbar,
